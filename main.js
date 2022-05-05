ad42fd0e59be3984d53ba6bb4976443b68505298
Vinicius Maclters <vinicius.maclters@hotmail.com>
2022-05-05 09:41:48 -0300

    Finalizado Aula 3

--- main.js
+++ main.js
@@ -13,3 +13,18 @@ function openMenu() {
 function closeMenu() {
   document.body.classList.remove('menu-expanded')
 }
+
+ScrollReveal({
+  distance: '30px',
+  origin: 'top',
+  duration: 700
+}).reveal(`
+  #home, 
+  #home img, 
+  #home .stats, 
+  #services,
+  #services header,
+  #services .card
+  #about,
+  #about header,
+  #about .content`);
